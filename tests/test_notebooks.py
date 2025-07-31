b71255313936f01222551d22b97333aad3ea11fe
maes <alice.sartorimonteirodebarros@ecmwf.int>
2025-07-31 13:06:16 +0200

    tmp

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -165,6 +165,8 @@ def test_changed_notebook(nb_file, nb_regression: NBRegressionFixture, caplog):
         if image_checks:
             filtered_diff = compare_images(result, image_checks_initial, image_checks_final, hash_distance_threshold=2)
             if filtered_diff:
+                logger.info(f"Initial: {result.nb_final.cells[10].outputs[0]}")
+                logger.info(f"Final: {result.nb_final.cells[10].outputs[0]}")
                 logger.info(f"Failed here Diff String:")  
                 final = diff_to_string(result.nb_final, filtered_diff, use_git=True, use_diff=True, use_color=True)
                 pytest.fail(final)
